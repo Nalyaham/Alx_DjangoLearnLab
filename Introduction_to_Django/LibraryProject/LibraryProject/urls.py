1f196656c3c6b795f94cb01e0dd9e4daae41bd67
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-07-14 00:23:07 +0300

    task2

--- Introduction_to_Django/LibraryProject/LibraryProject/urls.py
+++ Introduction_to_Django/LibraryProject/LibraryProject/urls.py
@@ -23,7 +23,3 @@ urlpatterns = [
 from django.contrib import admin
 from django.urls import include, path
 
-urlpatterns = [
-    path("books/", include("bookshelf.urls")),
-    path("admin/", admin.site.urls),
-]
---
+++ Introduction_to_Django/LibraryProject/bookshelf/create.md
@@ -0,0 +1,2 @@
+from bookshelf.models import Book
+book = Book.objects.create(title="1984", author="George Orwel",  publication_year= 1949)
