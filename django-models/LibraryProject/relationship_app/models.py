832e0f8913c18a43a8004fb99eed0008d1992775
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-07-23 18:58:55 +0300

    tas3

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,7 @@
 from django.db import models
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Author(models.Model):
@@ -11,6 +14,14 @@ class Book(models.Model):
     title = models.CharField
     author = models.ForeignKey(Author, on_delete=models.CASCADE)
 
+    class Meta:
+            permissions = [
+                 ('can_add_book', 'can add book'),
+                 ('can_change_book', 'can change book'), 
+                 ('can_delete_book', 'can delete book'),
+            ]
+
+
 class Library(models.Model):
     name = models.CharField
     books = models.ManyToManyField(Book,  related_name = 'book_in_library')
@@ -18,3 +29,10 @@ class Library(models.Model):
 class Librarian(models.Model):
     name = models.CharField
     library = models.OneToOneField(Library, on_delete=models.CASCADE, related_name='library')
+
+class UserProfile(models.Model): 
+    user = models.OneToOneField(User, on_delete=models.CASCADE) 
+    roles = [ ('ADMIN', 'Admin'), 
+             ('LIBRARIAN', 
+              'Librarian'), 
+              ('MEMBER' , 'member') ]
