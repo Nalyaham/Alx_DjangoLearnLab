0c1c22c0c1b37422c501815d5e9926d545fdfa9e
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-07-23 18:53:58 +0300

    tas3

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,6 +1,21 @@
 from django.urls import path
-from .views import list_books
+from .views import list_books, SignUpView, admin_view, member_view, librarian_view, add_book, edit_book, delete_book
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LogoutView
+
 
 urlpatterns = [
     path('booklist/', list_books.list_books, name='booklist'),
-    path('library/', list_books.LibraryDetailView.as_view(), name='library')]
+    path('library/', list_books.LibraryDetailView.as_view(), name='library'),
+    path('login/', LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'), 
+	path('register/', SignUpView.as_view(template_name = 'relationship_app/signup.html'), name='register'), 
+    path('Admin/', admin_view.admin_view, name='admin_view'), 
+    path('member/', member_view.member_view, name='member_view'), 
+    path('librarian/', librarian_view.librarian_view, name='librarian_view'),
+    path('AddBook/', add_book.add_book, name='add_book_view'),
+    path('EditBook/', edit_book.edit_book, name='edit_book_view'),
+    path('DeleteBook/', delete_book.delete_book, name='delete_book_view'),
+]
+
+
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views.generic.detail import DetailView
-from .models import Book
-from .models import Library 
+from .models import Book, Library, UserProfile
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.decorators import user_passes_test
+from django.contrib.auth.decorators import permission_required
+
 
 # Create your views here.
 def list_books(request):
@@ -16,4 +21,64 @@ class LibraryListView(DetailView):
 
     def get_queryset(self):
         return Library.objects.all().values('name', 'books')
+    
+class SignUpView(CreateView):
+     form_class = UserCreationForm 
+     success_url = reverse_lazy('login')
+     template_name = 'relationship_app/signup.html'
+
+def is_admin(user):    
+    return user.UserProfile.role == 'ADMIN'
+
+def is_librarian(user):
+     return user.UserProfile.role == 'LIBRARIAN'
+
+def is_member(user):  
+    return user.UserProfile.role == 'MEMBER'
+
+#View for the admin
+@user_passes_test(is_admin)
+def admin_view(request):
+    return render(request, 'admin_view.html')
+
+#View for the librarian 
+@user_passes_test(is_librarian)
+def librarian_view(request):
+    return render(request, 'librarian_view.html')
+
+#View for the member
+@user_passes_test(is_member)
+def member_view(request):
+    return render(request, 'member_view.html')
+
+#View for adding a book to the Book model
+@permission_required('relationship_app.can_add_book')
+def add_book(request):
+    if request.method == 'POST': 
+         name = request.POST['name']
+         author = request.POST['author']
+         Book.objects.create(name=name, author=author)
+         return render(request, 'add_book.html')
+
+#View for editing the book in Database 
+@permission_required('relationship_app.can_edit_book')
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+         book.name = request.POST['name']    
+         book.author = request.POST['author']
+         book.save()
+         return render(request, 'edit_book.html', {'book': book})
+    
+#View for deleting a book 
+@permission_required('relationship_app.can_delete_book')
+def edit_book(request, pk):
+    book = get_object_or_404(Book, pk=pk)
+    if request.method == 'POST':
+        book.delete()
+        return render(request, 'delete_book.html', {'book': book})
+
+
+
+
     
