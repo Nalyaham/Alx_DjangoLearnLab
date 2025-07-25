68ed1381809e9e02c95a050ccccf4ccdbd4ed913
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-07-25 15:07:53 +0300

    task1

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Library
-from django.contrib.auth import login
+from django.contrib.auth import loginview, logoutview
 from django.views.generic.detail import DetailView
 from .models import Book, Library, UserProfile
 from django.contrib.auth.forms import UserCreationForm
