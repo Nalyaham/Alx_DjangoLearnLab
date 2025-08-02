098b56544dcb6ce19032bdc572d7c2de081e6778
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-08-02 21:36:56 +0300

    task 0

--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-zq-&eu!d-gt!7ca+r9$bn^*^ppqsbavg-%s3!+u*#@vkd9rs5r"
+SECRET_KEY = "django-insecure-10o$#@28(pxsa-+z^pe=l1rd3yqsit5kv!7@)3j6$*vj3a!*h6"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "rest_framework", 
 ]
 
 MIDDLEWARE = [
