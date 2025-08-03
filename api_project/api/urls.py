33547637fef2efd520a43b5ed380e4c36c8c8099
Nalyanya Abraham <nalyanyaabraham1@gmail.com>
2025-08-03 13:15:34 +0300

    task 1

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('books/', BookList.as_view(), name='book-list'), 
-    path('api/', include("api.urls")), 
+    path('api/', include('api.urls')), 
 ]
